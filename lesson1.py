210f5da12aed47692b3c8f8b0c0c9a4fa0487fde
Cotne Kevkhishvili <tsotnekevkhishvili@gmail.com>
2023-09-07 01:57:24 +0400

    first commit

---
+++ lesson1.py
@@ -0,0 +1,87 @@
+from turtle import *
+width(7)
+color("purple")
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+#ოთხკუთხედის დასასრული
+
+
+#კარი
+
+forward(70)
+color("yellow")
+begin_fill()
+left(90)
+forward(120)
+right(90)
+forward(60)
+right(90)
+forward(120)
+end_fill()
+
+#კარის დასასრული
+
+#სახურავი
+
+penup()
+goto(200, 200)
+pendown()
+
+color("blue")
+begin_fill()
+right(150) 
+forward(200)
+left(120)
+forward(200)
+end_fill()
+
+#სახურავის დასასრული
+
+penup()
+goto(170, 170)
+pendown()
+
+#ფანჯარა
+
+color("brown")
+begin_fill()
+left(300)
+forward(30)
+left(90)
+forward(30)
+left(90)
+forward(30)
+left(90)
+forward(30)
+end_fill()
+
+penup()
+goto(30, 170)
+pendown()
+
+#ფანჯრები
+
+color("brown")
+begin_fill()
+right(90)
+forward(30)
+right(90)
+forward(30)
+right(90)
+forward(30)
+right(90)
+forward(30)
+end_fill()
+
+
+
+exitonclick()
